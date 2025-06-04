7f94df358654f252f6d370a5edfa3a79b5ecab51
G1tHub20 <a7m2cy@gmail.com>
2025-06-04 22:29:04 +0900

    ナビゲーションの軽微な修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -86,19 +86,12 @@
   <!-- Responsive Navigation Menu -->
   <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
     <div class="pt-2 pb-3 space-y-1">
-      {{-- <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link> --}}
-      {{-- <x-responsive-nav-link :href="route('movie.index')" :active="request()->routeIs('.index')">
-        TOP
-      </x-responsive-nav-link> --}}
     </div>
 
     <!-- Responsive Settings Options -->
     <div class="pt-4 pb-1 border-t border-gray-200">
       <div class="px-4">
-        <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-        {{-- <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div> --}}
+        <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}さん</div>
       </div>
 
       <div class="mt-3 space-y-1">
