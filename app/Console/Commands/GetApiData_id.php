756678c12daee7ae2e0c7a6d6d2aa63669df1715
G1tHub20 <a7m2cy@gmail.com>
2025-10-25 13:09:29 +0900

    映画データ取得APIの国名取得を修正

--- app/Console/Commands/GetApiData_id.php
+++ app/Console/Commands/GetApiData_id.php
@@ -73,85 +73,63 @@ class GetApiData_id extends Command
         $genres = implode(',', array_column($details['genres'], 'id'));
 
         //国名→国IDに置き換え
-        $countryName = isset($details['production_countries'][0]['name']) ? $details['production_countries'][0]['name'] : 'N/A';
-        $country = null;
-        switch ($countryName) {
-            case 'Japan':
-                $country = 1;
-                break;
-            case 'United States of America':
-                $country = 2;
-                break;
-            case 'United Kingdom':
-                $country = 3;
-                break;
-            case 'Italy':
-                $country = 4;
-                break;
-            case 'Spain':
-                $country = 5;
-                break;
-            case 'France':
-                $country = 6;
-                break;
-            case 'Germany':
-                $country = 7;
-                break;
-            case 'Canada':
-                $country = 8;
-                break;
-            case 'India':
-                $country = 9;
-                break;
-            case 'Australia':
-                $country = 10;
-                break;
-            case 'South Korea':
-                $country = 11;
-                break;
-            case 'China':
-                $country = 12;
-                break;
-            case 'Hong Kong':
-                $country = 13;
-                break;
-            default:
-                $country = 15;
-                break;
-        }
+        $countryCode = isset($details['production_countries'][0]['iso_3166_1']) ? $details['production_countries'][0]['iso_3166_1'] : 'N/A';
+        $countryMap = [
+            'JP' => 1,   // 日本
+            'US' => 2,   // アメリカ
+            'GB' => 3,   // イギリス
+            'IT' => 4,   // イタリア
+            'ES' => 5,   // スペイン
+            'FR' => 6,   // フランス
+            'DE' => 7,   // ドイツ
+            'CA' => 8,   // カナダ
+            'IN' => 9,   // インド
+            'AU' => 10,  // オーストラリア
+            'KR' => 11,  // 韓国
+            'CN' => 12,  // 中国
+            'HK' => 13,  // 香港
+            'BR' => 14,  // ブラジル
+            'MX' => 15,  // メキシコ
+            'FI' => 16,  // フィンランド
+            'BE' => 17,  // ベルギー
+            'DK' => 18,  // デンマーク
+        ];
 
-            // Movie
-            // もし重複するtmdb_idがあればスキップする
-            $count = Movie::where('tmdb_id', $details['id'])->count();
-            if($count < 1) {
-                $movie = new Movie;
-                $movie->title = $details['title'];
-                $movie->tmdb_id = $details['id'];
-                $movie->original_title = $details['original_title'];
-                $movie->overview = $details['overview'];
-                $movie->release_year = intval(substr($details['release_date'], 0, 4));
-                foreach($details['credits']['crew'] as $crew) {
-                    if($crew['known_for_department'] === 'Directing') {
-                        $movie->director = $crew['name'];
-                        break;
-                    }
-                }
-                $movie->country_id = $country;
-                $movie->image1 = $images[0];
-                $movie->image2 = $images[1];
-                var_dump($movie->getAttributes());
-                $movie->save(); //movieだけまず保存
+        $country = $countryMap[$countryCode] ?? 99;
 
-                // GenreMovie
-                $insertedId = $movie->id;
-                foreach(explode(",", $genres) as $genre_id) {
-                    $genreMovie = new GenreMovie;
-                    $genreMovie->movie_id = $insertedId;
-                    $genreMovie->genre_id = $genre_id;
-                    $genreMovie->save();
+        // Movie
+        // もし重複するtmdb_idがあればスキップする
+        $count = Movie::where('tmdb_id', $details['id'])->count();
+        if($count < 1) {
+            $movie = new Movie;
+            $movie->title = $details['title'];
+            $movie->tmdb_id = $details['id'];
+            $movie->original_title = $details['original_title'];
+            $movie->overview = $details['overview'];
+            $movie->release_year = intval(substr($details['release_date'], 0, 4));
+            // 監督名
+            foreach($details['credits']['crew'] as $crew) {
+                if($crew['known_for_department'] === 'Directing') {
+                    $movie->director = $crew['name'];
+                    break;
                 }
-            } else {
-                print("既に登録されています" . "（" . $details['id']. "）");
             }
+            $movie->country_id = $country;
+            $movie->image1 = $images[0];
+            $movie->image2 = $images[1];
+            var_dump($movie->getAttributes());
+            $movie->save(); //movieだけまず保存
+
+            // GenreMovie
+            $insertedId = $movie->id;
+            foreach(explode(",", $genres) as $genre_id) {
+                $genreMovie = new GenreMovie;
+                $genreMovie->movie_id = $insertedId;
+                $genreMovie->genre_id = $genre_id;
+                $genreMovie->save();
+            }
+        } else {
+            print("既に登録されています" . "（" . $details['id']. "）");
+        }
     }
 }
